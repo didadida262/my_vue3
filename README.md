338ffa80ccdef4a60d6a27a6076cb525318853e4
miles <719713496@qq.com>
2025-03-21 19:23:39 +0800

    init_project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my_vue3
+
 my_vue3_template
+基于 vue3+tailwind+ts 项目模板
---
+++ src/views/HomeComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-[200px] h-[200px] bg-[green]">
+    {{ msg }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const msg = ref('vue3_template')
+</script>
